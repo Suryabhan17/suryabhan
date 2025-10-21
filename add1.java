a531548c610ce851929632481a4914b5ca34c361
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-21 12:43:14 +0530

    new15

--- add1.java
+++ add1.java
@@ -1,6 +1,12 @@
 public class add1 {
   public static void main(String[] args) {
-    System.out.println("suryabhan Singh12245sdS");
+    System.out.println("suryabhan Singh12245sdSchedfgf");
   }
   
+}
+ class add3 {
+  public static void main(String[] args) {
+    System.out.println("suryabhan Singh1224545489");
+
+  }
 }
